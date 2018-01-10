f31a6fc6ae803000de50c0be605d9f83b4276c7e
jalilraziel <idv16c.jzavala@uartesdigitales.edu.mx>
2018-01-10 12:48:38 -0600

    +primer commit

---
+++ proyecto 1/proyecto 1/proyecto 1.cpp
@@ -0,0 +1,16 @@
+// proyecto 1.cpp: define el punto de entrada de la aplicación de consola.
+//
+
+#include "stdafx.h"
+
+struct MyStruct
+{
+	float X, Y, Z;
+
+};
+
+int main()
+{
+    return 0;
+}
+
